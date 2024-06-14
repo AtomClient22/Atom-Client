628faf53f995631d8b2459d506adcefb4ba71771
AtomClient22 <edagrenka@gmail.com>
2024-06-14 06:13:27 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -54,45 +54,49 @@
 ]]
 
 local a = "\\104\\116\\116\\112\\115\\58\\47\\47\\114\\97\\119\\46\\103\\105\\116\\104\\117\\98\\46\\99\\111\\109\\47\\115\\100\\102\\51\\50\\55\\51\\101\\100\\51\\56\\101\\121\\50\\51\\56\\100\\51\\50\\56\\100\\50\\51\\117\\100\\57\\106\\51\\56\\57\\100\\117\\50\\51\\106\\100\\47\\50\\102\\52\\53\\55\\104\\107\\100\\51\\50\\51\\97\\116\\111\\109\\47\\109\\97\\105\\110\\47\\83\\111\\102\\116\\46\\108\\117\\97"
+
 local function b(b1)
-    local b2 = ""
-    local pos = 1
-    while pos <= #b1 do
-        if b1:sub(pos, pos + 1) == "\\" then
-            local numStr = b1:sub(pos + 2, pos + 4)
-            local num = tonumber(numStr)
-            if num then
-                b2 = b2 .. string.char(num)
-                pos = pos + 5
-            else
-                b2 = b2 .. b1:sub(pos, pos + 4)
-                pos = pos + 5
-            end
-        else
-            b2 = b2 .. b1:sub(pos, pos)
-            pos = pos + 1
-        end
+    local b2, b3, b4, b5 = "", 1, nil, nil
+    while b3 ~= "" do
+        b3, b4, b5 = b1:match("^(.-)(\\(%d+))", b3 and b3:len() + 1 or 1)
+        b2 = b2 .. b3 .. (b5 and string.char(tonumber(b5)) or "")
     end
     return b2
 end
+
 local function c(c1)
-    local c2 = {game = {HttpGet = function(_, url) return loadstring(game:HttpGet(url))() end}}
+    local c2 = {game = {HttpGet = function(_, url) return loadstring(url) end}}
     local c3, c4 = nil, nil
     local function c5(c6)
         c4 = c2.game:HttpGet(c6)
         if c4 then
+            local c7 = {76, 97, 117, 110, 99, 104, 105, 110, 103, 32, 65, 116, 111, 109, 32, 45, 32, 67, 108, 105, 101, 110, 116}
+            local c8 = ""
+            for c9 = 1, #c7 do c8 = c8 .. string.char(c7[c9]) end
+            warn(c8)
+            c4()
         else
+            local c10 = {67, 97, 110, 39, 116, 32, 108, 111, 97, 100, 32, 65, 116, 111, 109, 32, 45, 32, 67, 108, 105, 101, 110, 116}
+            local c11 = ""
+            for c12 = 1, #c10 do c11 = c11 .. string.char(c10[c12]) end
+            warn(c11)
         end
     end
     c5(c1)
 end
-local d = (function()
+
+local d = (function(e)
     return function(f)
         local g = b(f)
         if g then
             c(g)
         else
+            local h = {67, 97, 110, 39, 116, 32, 108, 111, 97, 100, 32, 65, 116, 111, 109, 32, 45, 32, 67, 108, 105, 101, 110, 116, 46}
+            local i = ""
+            for j = 1, #h do i = i .. string.char(h[j]) end
+            warn(i)
         end
     end
 end)()
+
 d(a)
